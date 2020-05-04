2b4c3eeab156c717e0404d61451edf4382178d09
Daisuke Shimamoto <diskshima@gmail.com>
2020-05-04 14:29:29 +0900

    Read JSON file and print it out

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-pub fn run() {
-    // TODO: Open file.
+use std::error::Error;
+use std::fs;
 
-    // TODO: Parse JSON.
+pub struct Config {
+    pub filename: String,
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+
+    let json = json::parse(&content);
 
     // TODO: Stripout elements.
 
     // TODO: Print (for now).
+    println!("{:?}", json);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+use std::env;
+use std::process;
+
 use nbstripout_rust;
+use nbstripout_rust::Config;
 
 fn main() {
-    nbstripout_rust::run();
+    let config = match config_from_args() {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("Failed to parse arguments: {}", e);
+            process::exit(1);
+        }
+    };
+
+    if let Err(e) = nbstripout_rust::run(config) {
+        eprintln!("Errored with: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn config_from_args() -> Result<Config, &'static str> {
+    let mut args = env::args();
+
+    args.next();
+
+    let filename = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a file name"),
+    };
+
+    Ok(Config { filename })
 }
