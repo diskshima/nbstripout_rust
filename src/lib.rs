71bfb19564ddf04e42a2519508b6759751dde2bb
Daisuke Shimamoto <diskshima@gmail.com>
2020-05-04 14:52:04 +0900

    Add placeholder method for stripping and pretty print JSON

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 use std::fs;
 
+use json::JsonValue;
+
 pub struct Config {
     pub filename: String,
 }
@@ -8,12 +10,17 @@ pub struct Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
 
-    let json = json::parse(&content);
+    let json = json::parse(&content)?;
 
     // TODO: Stripout elements.
+    let json = stripout(json);
 
     // TODO: Print (for now).
-    println!("{:?}", json);
+    println!("{}", json::stringify_pretty(json, 1));
 
     Ok(())
+}
+
+fn stripout(json: JsonValue) -> JsonValue {
+    json
 }
