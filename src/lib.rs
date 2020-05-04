ecefa50e8b354f80bf98d83a02abb38aa22781a0
Daisuke Shimamoto <diskshima@gmail.com>
2020-05-04 17:12:56 +0900

    Add stripping of cell data

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 use std::fs;
 
+use json::array;
 use json::JsonValue;
+use json::object;
 
 pub struct Config {
     pub filename: String,
@@ -22,6 +24,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn stripout(json: JsonValue) -> JsonValue {
+fn stripout(mut json: JsonValue) -> JsonValue {
+    let cells = &mut json["cells"];
+
+    for cell in cells.members_mut() {
+        cell["outputs"] = array![];
+        cell["metadata"] = object!{};
+        cell["execution_count"] = JsonValue::Null;
+    }
+
     json
+    // TODO: Strip Google Colab
+    // TODO: Strip Google Colab accelerator
 }
