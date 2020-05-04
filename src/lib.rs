34a5d2a9a54ca9051f951fa7c1dc763fbfbf243e
Daisuke Shimamoto <diskshima@gmail.com>
2020-05-04 17:51:23 +0900

    Add command line arguments for stripping

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,18 @@ use json::JsonValue;
 use json::object;
 
 pub struct Config {
+    pub colab: bool,
+    pub execution_count: bool,
     pub filename: String,
+    pub outputs: bool,
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.filename)?;
+    let content = fs::read_to_string(&config.filename)?;
 
     let json = json::parse(&content)?;
 
-    let json = stripout(json);
+    let json = stripout(json, &config);
 
     // TODO: Add option to configure whitespaces.
     println!("{}", json::stringify_pretty(json, 1));
@@ -24,19 +27,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn stripout(mut json: JsonValue) -> JsonValue {
+fn stripout(mut json: JsonValue, config: &Config) -> JsonValue {
     let cells = &mut json["cells"];
 
     for cell in cells.members_mut() {
-        cell["outputs"] = array![];
         cell["metadata"] = object!{};
-        cell["execution_count"] = JsonValue::Null;
+
+        if config.outputs {
+            cell["outputs"] = array![];
+        }
+
+        if config.execution_count {
+            cell["execution_count"] = JsonValue::Null;
+        }
     }
 
     let metadata = &mut json["metadata"];
 
-    metadata.remove("accelerator");
-    metadata.remove("colab");
+    if config.colab {
+        metadata.remove("accelerator");
+        metadata.remove("colab");
+    }
 
     json
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::env;
 use std::process;
+
+use clap::{Arg, App};
 
 use nbstripout_rust;
 use nbstripout_rust::Config;
@@ -21,14 +22,32 @@ fn main() {
 }
 
 fn config_from_args() -> Result<Config, &'static str> {
-    let mut args = env::args();
+   let matches = App::new("nbstripout-rust")
+                          .version("0.1.0")
+                          .author("Daisuke Shimamoto <diskshima@gmail.com>")
+                          .about("nbstripout implemented in Rust")
+                          .arg(Arg::with_name("colab")
+                               .short('c')
+                               .long("colab")
+                               .about("Strip colab"))
+                          .arg(Arg::with_name("execution_count")
+                               .short('e')
+                               .long("execution-count")
+                               .about("Strip execution_count"))
+                          .arg(Arg::with_name("outputs")
+                               .short('o')
+                               .long("outputs")
+                               .about("Strip outputs"))
+                          .arg(Arg::with_name("input_file")
+                               .about("Sets the input file to use")
+                               .required(true)
+                               .index(1))
+                          .get_matches();
 
-    args.next();
+    let colab = matches.is_present("colab");
+    let execution_count = matches.is_present("execution_count");
+    let outputs = matches.is_present("outputs");
+    let filename = matches.value_of("input_file").unwrap().to_string();
 
-    let filename = match args.next() {
-        Some(arg) => arg,
-        None => return Err("Didn't get a file name"),
-    };
-
-    Ok(Config { filename })
+    Ok(Config { colab, execution_count, filename, outputs })
 }
